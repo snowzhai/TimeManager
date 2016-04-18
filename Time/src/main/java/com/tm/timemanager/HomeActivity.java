9c91724a524547b39c3b700ade667984800155d6
ld407553497 <407553497@qq.com>
2016-04-18 19:44:43 +0800

    Update HomeActivity.java

--- Time/src/main/java/com/tm/timemanager/HomeActivity.java
+++ Time/src/main/java/com/tm/timemanager/HomeActivity.java
@@ -8,6 +8,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_home);//1234555
     }
 }
